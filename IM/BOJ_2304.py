cd8cd552aea665debdc296318fe34d651f786b38
oth54477 <oth5447@naver.com>
2022-08-12 01:47:38 +0900

    new

---
+++ IM/BOJ_2304.py
@@ -0,0 +1,11 @@
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+# cnt = 0
+pop_arr = []
+arr.sort()
+new_arr = sorted(arr)
+for i in range(1, n - 1):
+    if arr[i][1] <= arr[i + 1][1] and arr[i][1] <= arr[i - 1][1]:
+        pop_arr.append(i)
+
+print(pop_arr)

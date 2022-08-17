63f18732ecbd4fdd14faa63199b8916ac40c2a3c
oth54477 <oth5447@naver.com>
2022-08-18 00:21:53 +0900

    asd

---
+++ Stack1/2005. 파스칼의 삼각형/s2.py
@@ -0,0 +1,18 @@
+import sys
+
+sys.stdin = open('input.txt')
+
+for t in range(1, int(input()) + 1):
+    print(f'#{t}')
+    n = int(input())
+    cnt = 0
+    arr = [[] for _ in range(n)]
+
+    while cnt < n:
+        for i in range(cnt + 1):
+            if i - 1 < 0 or i + 1 > cnt:
+                arr[cnt].append(1)
+            else:
+                arr[cnt].append(arr[cnt - 1][i] + arr[cnt - 1][i - 1])
+        print(*arr[cnt])
+        cnt += 1
